20462000dfe904043027b1b22ec41ddf685c6a9b
Nick Couchman <nick_couchman@cotyinc.com>
2019-02-09 12:13:25 -0500

    Duplicate fix duplicate fields.

--- src/main/java/org/apache/guacamole/auth/docker/user/DockerStartupUser.java
+++ src/main/java/org/apache/guacamole/auth/docker/user/DockerStartupUser.java
@@ -89,7 +89,7 @@ public class DockerStartupUser extends DelegatingUser {
                                .map(GuacamoleProtocol::name)
                                .collect(Collectors.toList())),
                     new NumericField(DOCKER_IMAGE_PORT_ATTRIBUTE),
-                    new TextField(DOCKER_IMAGE_NAME_ATTRIBUTE)
+                    new TextField(DOCKER_IMAGE_CMD_ATTRIBUTE)
             ));
     
     /**
